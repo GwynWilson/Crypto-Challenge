750a5ca4af6c8267e9ee5d5ebbc2d3cd3710d836
Gwyn Wilson <gwilson97@hotmail.co.uk>
2017-06-27 17:05:12 +0100

    Started challenge 3

---
+++ Set 1/Challenge_3.py
@@ -0,0 +1,13 @@
+from Challenge_1 import *
+import binascii
+input = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+input_10 = convertTo10(input,16)
+print ('Input in base 10:',input_10)
+
+# getText = "686974207468652062756c6c277320657965"
+
+# x = [str(x)+str(y) for x,y in zip(*[iter(getText)]*2)]
+# print (x)
+
+
+# print(binascii.unhexlify(input))
