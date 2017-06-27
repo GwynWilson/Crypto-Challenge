51a2ebdef6b04a4a282ee7e736ed71fcd1a1d0a7
Gwyn Wilson <gwilson97@hotmail.co.uk>
2017-06-27 17:50:07 +0100

    General method for doing xor with a key

--- Set 1/Challenge_3.py
+++ Set 1/Challenge_3.py
@@ -1,13 +1,18 @@
 from Challenge_1 import *
 import binascii
 input = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-input_10 = convertTo10(input,16)
-print ('Input in base 10:',input_10)
 
 # getText = "686974207468652062756c6c277320657965"
+# getText_data = binascii.unhexlify(getText)
+# print (binascii.b2a_qp(getText_data))
 
-# x = [str(x)+str(y) for x,y in zip(*[iter(getText)]*2)]
-# print (x)
+input_data = binascii.unhexlify(input)
 
-
-# print(binascii.unhexlify(input))
+key = ord('a')
+print (key)
+       
+decrypt = [chr(byte^key) for byte in input_data]
+out = ''
+for ch in decrypt:
+    out += ch
+print (out)
